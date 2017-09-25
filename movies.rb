42cef4ff19d38252676ba786f501a8ba1513816b
novokshonovp <novokshonovp@gmail.com>
2017-09-25 15:01:06 +0100

    Include zip library, change each+push to .map method, use count method with code block

--- movies.rb
+++ movies.rb
@@ -1,52 +1,63 @@
-C_FILENAME = "movies.txt"
+C_FILENAME_ZIP = "movies.txt.zip"
+C_FILENAME_TXT = "movies.txt"
+C_LINK = :link
+C_TITLE = :title
+C_RELEASE_YEAR = :r_year
+C_COUNTRY = :country
+C_RELEASE_DATE = :r_date
+C_GENRE = :genre
+C_RUNTIME = :runtime
+C_RATING = :rating
+C_DIRECTOR = :director
+C_STARS = :stars
+
+require 'zip'
+
 # method to print a movie data
 def print_movies(a_movies,i_records)
 a_movies[0..i_records].each {| i | 
 						puts "#{i[:title]} (#{i[:r_date]}; #{i[:genre]}) - #{i[:runtime]} min"}	
 end
+#check params
 filename = ARGV[0]
 a_movies = Array.new
 if ARGV.length<1
-	puts "Too few arguments. Use the default #{C_FILENAME}."
-	filename = C_FILENAME
+	puts "Too few arguments. Use the default #{C_FILENAME_ZIP}."
+	filename = C_FILENAME_ZIP
 end
 if !File.exist?(filename)
-	puts "File #{filename} doesn't exist. Use the default #{C_FILENAME}."
-	filename = C_FILENAME
+	puts "File #{filename} doesn't exist. Use the default #{C_FILENAME_ZIP}."
+	filename = C_FILENAME_ZIP
 end
-File.open(filename).each do |record|
-	#parse file to array structure
-	a_movies.push({:link => record.split("|")[0],:title =>record.split("|")[1],
-					:r_year => record.split("|")[2], 
-					:country => record.split("|")[3], 
-					:r_date => record.split("|")[4],
-					:genre => record.split("|")[5],
-					:runtime => record.split("|")[6].scan(/[0-9]/).join.to_i,
-					:rating => record.split("|")[7],
-					:director => record.split("|")[8],
-					:stars => record.split("|")[9].chop}) 
-end
+#parse zip file 
+a_movies = Zip::File.new(C_FILENAME_ZIP).read(C_FILENAME_TXT).split("\n").map {
+						|record| 
+					Hash[C_LINK => record.split("|")[0],
+					C_TITLE =>record.split("|")[1], 
+					C_RELEASE_YEAR => record.split("|")[2], 
+					C_COUNTRY => record.split("|")[3], 
+					C_RELEASE_DATE => record.split("|")[4],
+					C_GENRE => record.split("|")[5],
+					C_RUNTIME => record.split("|")[6].scan(/[0-9]/).join.to_i,
+					C_RATING => record.split("|")[7],
+					C_DIRECTOR => record.split("|")[8],
+					C_STARS => record.split("|")[9]]}
+
+
 #print the 5 longest movies
 puts "###   The 5 longest movies   ###"
-t_movies = a_movies.sort_by {|i| 
-				i[:runtime] 
-				}.reverse
-print_movies(t_movies,5)
+print_movies(a_movies.sort_by {|i| i[C_RUNTIME] }.reverse,5)
 
 #print 10 comedies ordered by r_date
 puts "###   10 comedies ordered by release date   ###"
-t_movies = a_movies.select {|i| i[:genre].include? "Comedy" }.sort_by{ |i| i[:r_date] }
-print_movies(t_movies,10)
+print_movies(a_movies.select {|i| i[C_GENRE].include? "Comedy" }.sort_by{ |i| i[C_RELEASE_DATE] },10)
 
 #print all directors ordered by last word
 puts "###   all directors ordered by last word   ###"
-a_directors = Array.new
-a_movies.sort_by {|i| i[:director].split.last}.uniq{ |i| i[:director]}.each{|i| a_directors.push(i[:director])}
-puts a_directors
+puts a_movies.sort_by {|i| i[C_DIRECTOR].split.last}.map {|i| [i[C_DIRECTOR]]}.uniq
 
 #print the number of the movies shoted not in the USA
-puts "\nThe number of the movies shoted not in the USA: " + a_movies.select {|i| !i[:country].include? "USA" }.count.to_s
-						
+puts "\nThe number of the movies shoted not in the USA: " + a_movies.count {|i| !i[C_COUNTRY].include? "USA" }.to_s						
 						
 						
 						
