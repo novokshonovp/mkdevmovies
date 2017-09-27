709701dbade0ca292608ff10180b3bafb310aad2
novokshonovp <novokshonovp@gmail.com>
2017-09-28 00:50:53 +0100

    Rebuild app to use clean ruby statements

--- movies.rb
+++ movies.rb
@@ -1,8 +1,8 @@
 
-C_HASH_DEFS = {filename_zip: "movies.txt.zip", 
+DEFS = {filename_zip: "movies.txt.zip", 
             filename_txt: "movies.txt", 
             country: "USA"}
-C_HASH_DATA_STRUCTURE = %i[link title r_year country r_date genre runtime rating director stars]
+DATA_STRUCTURE = %i[link title r_year country r_date genre runtime rating director stars]
 
 require 'zip'
 require 'csv'
@@ -18,55 +18,43 @@ end
 #check params
 filename = ARGV[0]
 if ARGV.length<1
-    puts "Too few arguments. Use the default #{C_HASH_DEFS[:filename_zip]}."
-    filename = C_HASH_DEFS[:filename_zip]
+    puts "Too few arguments. Use the default #{DEFS[:filename_zip]}."
+    filename = DEFS[:filename_zip]
 end
 if !File.exist?(filename)
-    puts "File #{filename} doesn't exist. Use the default #{C_HASH_DEFS[:filename_zip]}."
-    filename = C_HASH_DEFS[:filename_zip]
+    puts "File #{filename} doesn't exist. Use the default #{DEFS[:filename_zip]}."
+    filename = DEFS[:filename_zip]
 end
 #read zip file 
-zip_file = Zip::File.new(C_HASH_DEFS[:filename_zip]).read(C_HASH_DEFS[:filename_txt])
+zip_file = Zip::File.new(DEFS[:filename_zip]).read(DEFS[:filename_txt])
 #parse data to array of OpenStruct objects
-a_movies = CSV.parse(zip_file,:col_sep=>"|").map { |i| rowdata = OpenStruct.new(C_HASH_DATA_STRUCTURE.zip(i).to_h) }
-
+movies = CSV.parse(zip_file,:col_sep=>"|",:headers=>DATA_STRUCTURE).map{ |i| OpenStruct.new(i.to_h) }
 #print the 5 longest movies
 puts "###   The 5 longest movies   ###"
-print_movies(a_movies.sort_by {|i| i.runtime.scan(/[0-9]/).join.to_i }.reverse,5)
+print_movies(movies.sort_by { |movie| movie.runtime.scan(/[0-9]/).join.to_i }.reverse,5)
 
 #print 10 comedies ordered by r_date
 puts "###   10 comedies ordered by release date   ###"
-print_movies(a_movies.select {|i| i.genre.include? "Comedy" }.sort_by{ |i| i.r_date },10)
+print_movies( movies.select {|movie| movie.genre.include? "Comedy" }.sort_by(&:r_date),10)
 
 
 #print all directors ordered by last word
 puts "###   all directors ordered by last word   ###"
-puts a_movies.sort_by {|i| i.director.split.last}.map {|i| [i.director]}.uniq
+puts movies.sort_by { |movie| movie.director.split.last }.map(&:director).uniq
 
 
 #print the number of the movies shooted not in the USA
-puts "\nThe number of the movies shoted not in the USA: #{a_movies.count {|i| !i.country.include? C_HASH_DEFS[:country] }.to_s} \n\n"                      
+puts "\nThe number of the movies shoted not in the USA: #{ movies.count {|movie| !movie.country.include? DEFS[:country] }.to_s} \n\n"                      
 
 #print number of movies shooted by months      
 puts "###   number of movies shooted by months   ###"   
-x = a_movies.inject(Hash.new(0)) { |h,i| 
-                                    #get and check month data
-                                    begin
-                                      month = Date.parse(i.r_date).month
-                                    rescue ArgumentError
-                                      if /-(\d+)/.match?(i.r_date.to_s)
-                                        month = /-(\d+)/.match(i.r_date.to_s)[1]
-                                        puts "The movie \"#{i.title}\" has incorrect date - release date (#{i.r_date}), use #{month} as a month"
-                                      else
-                                        puts "The movie \"#{i.title}\" has no month date - release date (#{i.r_date})"                                       
-                                      end
-                                    end
-                                    if !month.nil? 
-                                      h[month.to_i]+=1 
-                                    end
-                                    h
-                                  }
-x.sort_by { |i| i[0]}.each {|i| puts "The number of the movies shooted in #{Date::MONTHNAMES[i[0]]}: #{i[1]}"  }
+
+movies
+    .reject{ |movie| movie.r_date.count('-').zero? }
+    .each_with_object(Hash.new(0)){ |movie,stats| stats[Date.strptime(movie.r_date,"%Y-%m").month]+=1 }
+    .sort
+    .each{ |month,stats|  puts "The number of the movies shooted in #{Date::MONTHNAMES[month]}: #{stats}" }    
 
 
-     
+
+
